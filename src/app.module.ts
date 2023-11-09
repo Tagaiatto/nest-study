b55e7f5f493d218807a9321ef4c17092c9c2f046
Tagaiatto <edilsonmneto@gmail.com>
2023-11-09 19:07:48 -0300

    created UserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 
 @Module({
   imports: [],
-  controllers: [UserController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
