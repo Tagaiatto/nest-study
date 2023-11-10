70eac5050ed8d694aa9a15dfe3bbb9c26f532aef
Tagaiatto <edilsonmneto@gmail.com>
2023-11-10 18:47:58 -0300

    added return of index of added element into users

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -4,8 +4,9 @@ import { Injectable } from "@nestjs/common";
 export class UserRepository {
     private users = [];
 
-    async save(user) {
-        this.users.push(user);        
+    async save(user): Promise<number> {
+        this.users.push(user);
+        return this.users.length - 1;        
     }
 
     async list() {
