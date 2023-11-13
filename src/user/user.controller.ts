14d46991279692232dd3b261a1c8635c5b33b9c2
Edilson Mendes <edilsonmneto@gmail.com>
2023-11-13 12:40:33 -0300

    added dto to UserController, added class-validator and class-transformer

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { UserRepository } from "./user.repository";
+import { CreateUserDTO } from "./dto/CreateUser.dto";
 
 @Controller('/users')
 export class UserController{
     constructor(private userRepository: UserRepository){}    
     
     @Post()
-    async createUser(@Body() userData) {
+    async createUser(@Body() userData: CreateUserDTO) {
         const id = await this.userRepository.save(userData);
         return `ID = ${id}`;        
     }
