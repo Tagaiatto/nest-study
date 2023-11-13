e9474f24db16a2e3fb97d15208d933a77e66a4c8
Edilson Mendes <edilsonmneto@gmail.com>
2023-11-13 17:15:50 -0300

    added productdto dependency to productcontroller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { ProductRepository } from "./product.repository";
+import { CreateProductDTO } from "./dto/CreateProduct.dto";
 
 @Controller('/products')
 export class ProductController{
     constructor(private productRepository: ProductRepository){}    
     
     @Post()
-    async createUser(@Body() productData) {
+    async createUser(@Body() productData: CreateProductDTO) {
         const id = await this.productRepository.save(productData);
         return `ID = ${id}`;
     }
