b98a3a8e197e126c0435672f03650c2b48c1e450
Tagaiatto <edilsonmneto@gmail.com>
2023-12-06 20:44:58 -0300

    create fn fix

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post, Get } from "@nestjs/common";
 import { ProductRepository } from "./product.repository";
 import { CreateProductDTO } from "./dto/CreateProduct.dto";
+import { v4 as uuid } from 'uuid';
 
 @Controller('/products')
 export class ProductController{
     constructor(private productRepository: ProductRepository){}    
     
     @Post()
-    async createUser(@Body() productData: CreateProductDTO) {
+    async createProduct(@Body() productData: CreateProductDTO) {
         const id = await this.productRepository.save(productData);
         return `ID = ${id}`;
     }
